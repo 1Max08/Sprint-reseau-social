f240c2de0008c64b1e81f4e98ba2f49d1d8a1683
angel <angelika.lavrentiev@gmail.com>
2025-10-15 15:13:26 +0200

    Update et delet des message et debut et debug de dark mode et burger menu

--- assets/app.js
+++ assets/app.js
@@ -7,15 +7,6 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
-
 /* Dark mode */
 document.querySelector('#theme').addEventListener('click', () => {
         document.documentElement.classList.toggle('dark');
